10621a2fb5f07fab56feed1c9bb6e81d40ca41e2
Shane <shane_matsumura@berkeley.edu>
2026-04-24 13:20:55 -0700

    initialized TT, still need to instantiate, define methods, etc

--- src/transposition.cpp
+++ src/transposition.cpp
@@ -1,1 +1,25 @@
 #include "transposition.h"
+
+void TranspositionTable::resize(size_t mb) {
+
+}
+
+//clear TT
+void TranspositionTable::clear() {
+    //loop over all TT elements
+    for (int index = 0; index < HASH_SIZE; ++index) {
+        //clear all attributes
+        table[index].hash_key = 0;
+        table[index].depth = 0;
+        table[index].flag = 0;
+        table[index].score = 0;
+    }
+}
+
+void TranspositionTable::store(uint64_t key, int depth, int value, int flag, Move best) {
+
+}
+
+bool TranspositionTable::probe(uint64_t key, int depth, int alpha, int beta, int& value, Move& best) const {
+    return false;
+}
--- src/transposition.h
+++ src/transposition.h
@@ -4,6 +4,9 @@
 #include "zobrist.h"
 #include "board.h"
 
+//hash table size
+#define HASH_SIZE 0x400000
+
 enum HashFlag {
     HASH_EXACT = 0,
     HASH_ALPHA = 1,
@@ -11,13 +14,23 @@ enum HashFlag {
 };
 
 struct TTEntry {
-    uint64_t key = 0;
+    uint64_t hash_key = 0;
     int depth = 0;
-    int flag = HASH_EXACT;
-    int value = 0;
-    Move best{};
+    int flag = HASH_EXACT;  //fail-low, fail-high, PV
+    int score = 0;          //score (alpha/beta/PV)
+    //Move best{};            //best move
 };
 
+//define TT
+class TranspositionTable {
+    public:
+        void resize(size_t mb);
+        void clear();
+        void store(uint64_t key, int depth, int value, int flag, Move best);
+        bool probe(uint64_t key, int depth, int alpha, int beta, int& value, Move& best) const;
 
+    private:
+        TTEntry table[HASH_SIZE];
+};
 
 #endif
