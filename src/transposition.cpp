c709e8126bcdb5ccba1ab7ab922463b032dde67a
Shane <shane_matsumura@berkeley.edu>
2026-04-24 15:19:22 -0700

    changed key collision so any diff position or lower depth is replaced

--- src/transposition.cpp
+++ src/transposition.cpp
@@ -18,8 +18,13 @@ void TranspositionTable::clear() {
 
 void TranspositionTable::store(uint64_t key, int depth, int value, int flag) {
     TTEntry& e = table[key % HASH_SIZE];
+
+    //if TT entry is already deeper
+    if (e.hash_key == key && e.depth > depth) {
+        return;
+    }
     
-    //write hash entry data
+    //overwrite hash entry data
     e.hash_key = key;
     e.depth = depth;
     e.score = value;
@@ -31,12 +36,13 @@ bool TranspositionTable::probe(uint64_t key, int depth, int alpha, int beta, int
     const TTEntry& e = table[key % HASH_SIZE];
     
     //ensure the position matches
-    if (e.hash_key == key) {
+    if (e.hash_key == key && e.depth >= depth) {
         //PV node score
-        if (e.flag == HASH_EXACT) {
+        if (e.flag == HASH_EXACT || (e.flag == HASH_ALPHA && e.score <= alpha) || (e.flag == HASH_BETA && e.score >= beta)) {
             value = e.score;
             return true;
         }
+        /*
         //fail-low
         if (e.flag == HASH_ALPHA && e.score <= alpha) {
             value = alpha;
@@ -47,6 +53,7 @@ bool TranspositionTable::probe(uint64_t key, int depth, int alpha, int beta, int
             value = beta;
             return true;
         }
+        */
     }
     //entry doesn't exist
     return false;
